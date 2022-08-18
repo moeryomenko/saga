324b9f40b5a430979da3283eaf92c14f46a59e06
Maxim Eryomenko <moeryomenko@gmail.com>
2022-08-18 16:05:32 +0300

    fix(payment): balance database model
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- migrations/payment/000001_initial_schema.up.sql
+++ migrations/payment/000001_initial_schema.up.sql
@@ -2,14 +2,15 @@ CREATE TYPE payment_status AS ENUM ('new', 'completed', 'canceled');
 
 CREATE TABLE IF NOT EXISTS balances (
 	customer_id      UUID UNIQUE NOT NULL,
-	available_amount DECIMAL DEFAULT NULL,
-	reserved_amount  DECIMAL DEFAULT NULL,
+	available_amount DECIMAL     NOT NULL DEFAULT 0,
+	reserved_amount  DECIMAL     NOT NULL DEFAULT 0,
 	PRIMARY KEY(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS payments (
 	payment_id  UUID UNIQUE    NOT NULL,
 	customer_id UUID           NOT NULL,
+	order_id    UUID UNIQUE    NOT NULL,
 	amount      DECIMAL        NOT NULL,
 	status      payment_status NOT NULL DEFAULT 'new',
 	PRIMARY KEY(payment_id),
